71232b857f4da35a67cfaa26c6dbbc1aac68f105
e7iwwru7tpaa2igq <99442689+e7iwwru7tpaa2igq@users.noreply.github.com>
2022-02-23 19:26:50 +0900

    Update Kirin_CP.js

--- Kirin_CP.js
+++ Kirin_CP.js
@@ -28,7 +28,7 @@
 
     setTimeout(() => {
         Press("div[class='login_button login_button_ln'] a",3);
-        VideoEnd("video[id='entry_movie']",0);
-        Press("a[id='em_button']",);
+        VideoEnd("video[id='entry_movie']");
+        Press("a[id='em_button']",1);
     }, 100);
 })();
