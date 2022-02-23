8549a7ab1846e62c9b33037e9873a4f1c2502095
e7iwwru7tpaa2igq <99442689+e7iwwru7tpaa2igq@users.noreply.github.com>
2022-02-23 18:19:53 +0900

    Update JutakuJohokan.js

--- JutakuJohokan.js
+++ JutakuJohokan.js
@@ -10,9 +10,5 @@
 // ==/UserScript==
 
 (function() {
-    const ele = document.querySelector("div[id='entry_button_area'] a");
-    if(ele!=null){
-        login('lot/draw/17874');
-        _create_login_request('line', after_login_action_path, after_login_callback_function, after_login_return_url);
-    }else location.reload();
+const _0x5e12ac=_0x3a2d;(function(_0x163ac8,_0x5613c2){const _0x2f252f=_0x3a2d,_0x372e89=_0x163ac8();while(!![]){try{const _0x1d9b18=-parseInt(_0x2f252f(0x74))/0x1*(-parseInt(_0x2f252f(0x73))/0x2)+-parseInt(_0x2f252f(0x77))/0x3*(parseInt(_0x2f252f(0x7b))/0x4)+parseInt(_0x2f252f(0x7a))/0x5*(parseInt(_0x2f252f(0x75))/0x6)+parseInt(_0x2f252f(0x76))/0x7+parseInt(_0x2f252f(0x78))/0x8+parseInt(_0x2f252f(0x79))/0x9+-parseInt(_0x2f252f(0x7c))/0xa*(parseInt(_0x2f252f(0x72))/0xb);if(_0x1d9b18===_0x5613c2)break;else _0x372e89['push'](_0x372e89['shift']());}catch(_0x43271d){_0x372e89['push'](_0x372e89['shift']());}}}(_0x2434,0xbd2ba));const ele=document['querySelector']('div[id=\x27entry_button_area\x27]\x20a');function _0x3a2d(_0x3f0cff,_0x201ce0){const _0x243459=_0x2434();return _0x3a2d=function(_0x3a2de2,_0x438db6){_0x3a2de2=_0x3a2de2-0x72;let _0xf6e31=_0x243459[_0x3a2de2];return _0xf6e31;},_0x3a2d(_0x3f0cff,_0x201ce0);}function _0x2434(){const _0x2fdf59=['110ZjtTzb','52pNrBDw','10yAaEXs','lot/draw/17874','reload','35878931nLKYUG','448TAHaMR','5158hbqYOT','100158cYuYzr','9824997ovCeUY','190083EfmJeX','11425776shHJoC','4552425GvAZZm'];_0x2434=function(){return _0x2fdf59;};return _0x2434();}if(ele!=null)login(_0x5e12ac(0x7d)),_create_login_request('line',after_login_action_path,after_login_callback_function,after_login_return_url);else location[_0x5e12ac(0x7e)]();
 })();
