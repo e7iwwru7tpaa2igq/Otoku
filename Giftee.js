9d7ee36f0d591bdf3c650ae404f39420d0428525
e7iwwru7tpaa2igq <99442689+e7iwwru7tpaa2igq@users.noreply.github.com>
2022-04-04 11:15:19 +0900

    Update Giftee.js

--- Giftee.js
+++ Giftee.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         Auto Kirin Campaign
+// @name         Auto Giftee
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://cp.kirin.jp/lot/*
-// @icon         https://www.google.com/s2/favicons?domain=cp.kirin.jp
+// @match        https://gcp.giftee.biz/*
+// @icon         https://www.google.com/s2/favicons?domain=gcp.giftee.biz
 // @grant        none
 // ==/UserScript==
 
