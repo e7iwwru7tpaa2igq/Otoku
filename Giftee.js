ff632af87032368c78e97607b0a6d7f268267b32
e7iwwru7tpaa2igq <99442689+e7iwwru7tpaa2igq@users.noreply.github.com>
2022-04-05 22:30:57 +0900

    Update Giftee.js

--- Giftee.js
+++ Giftee.js
@@ -5,20 +5,27 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://gcp.giftee.biz/*
-// @icon         https://www.google.com/s2/favicons?domain=gcp.giftee.biz
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=gcp.giftee.biz
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    function Press(ele,sec=0){
-        const obj = document.querySelector(ele);
+    function ObjXPath(str){
+        const obj = document.evaluate(str,document,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
 
-        if(obj!=null){
+        if(!obj===null){
+            return obj.snapshotItem(0);
+        }else return null;
+    };
+
+    ObjXPath("//a[.//span[contains(text(),'応募する')]]").click();
+/*
+    function Press(ele,sec=0){
+        if(!ele===null){
             setTimeout(() => {
-                obj.click();
+                ele.click();
             }, parseInt(sec)*1000);
         }
     }
-
-    Press("a[class='ui labeled icon primary button line-button ']");
+*/
 })();
